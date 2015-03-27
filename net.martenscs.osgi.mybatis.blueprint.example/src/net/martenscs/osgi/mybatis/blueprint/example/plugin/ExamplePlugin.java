668e2bc94d2dbea907b1cb3b3f170d3d90633ccf
cmartens <cmartens@DAL-CMARTENS1.tmwsystems.com>
2015-03-27 13:29:26 -0500

    Played with Plugin

--- net.martenscs.osgi.mybatis.blueprint.example/src/net/martenscs/osgi/mybatis/blueprint/example/plugin/ExamplePlugin.java
+++ net.martenscs.osgi.mybatis.blueprint.example/src/net/martenscs/osgi/mybatis/blueprint/example/plugin/ExamplePlugin.java
@@ -1,18 +1,20 @@
 package net.martenscs.osgi.mybatis.blueprint.example.plugin;
 
+import java.sql.Statement;
 import java.util.Properties;
 
 import org.apache.ibatis.executor.Executor;
-import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.executor.resultset.ResultSetHandler;
+import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Plugin;
 import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.session.ResultHandler;
 
 // ExamplePlugin.java
-@Intercepts({ @Signature(type = Executor.class, method = "update", args = {
-		MappedStatement.class, Object.class }) })
+@Intercepts({ @Signature(type = StatementHandler.class, method = "query", args = {  Statement.class ,ResultHandler.class}) })
 public class ExamplePlugin implements Interceptor {
 	public Object intercept(Invocation invocation) throws Throwable {
 		return invocation.proceed();
